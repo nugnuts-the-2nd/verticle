433bc5b76b7d14f7d338038fbe57c4c2f672f0c1
Daxton Brown <daxton177@gmail.com>
2026-01-17 18:38:12 -0800

    Change letters building

--- verticle.py
+++ verticle.py
@@ -15,14 +15,7 @@ WORD_LENGTH = 5  # needs to be length of the words
 
 game_board = ""
 
-letters = {
-        'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0,
-        'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0,
-        'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 0,
-        'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0,
-        'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0,
-        'Z': 0
-    }
+letters = {chr(i): 0 for i in range(ord('A'), ord('Z') + 1)}
 
 
 def main():
